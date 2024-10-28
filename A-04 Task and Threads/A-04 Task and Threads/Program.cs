3a08256ad85285c7cdc2c1afff091695ff2c0a66
Juan Jose Bejarano <juanjosebejarano@hotmail.com>
2024-10-28 17:18:44 -0400

    third commit on this commit, I had a file check (File.Exists) and a confirmation prompt

--- A-04 Task and Threads/A-04 Task and Threads/Program.cs
+++ A-04 Task and Threads/A-04 Task and Threads/Program.cs
@@ -22,35 +22,28 @@ namespace A_04_Task_and_Threads
                 Console.WriteLine("Invalid Size\nGive a number between 1000 and 20,000,000");
                 return;
             }
-            if (!int.TryParse(args[1], out int numTask) || numTask <= 1 || numTask >= 25)
+            if (!int.TryParse(args[2], out int numTask) || numTask <= 1 || numTask >= 25)
             {
                 Console.WriteLine("Invalid Task number\nGive a number between 1 and 25");
                 return;
             }
+
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine($"File: {fileName}");
+                string response = Console.ReadLine();
+                if (response?.ToLower() != "y")
+                {
+                    Console.WriteLine("Operation canceled");
+                    return;
+                }
+            }
+
             Console.WriteLine($"Filename: {fileName}");
             Console.WriteLine($"File Size: {fileSize}");
             Console.WriteLine($"Number of Tasks: {numTask}");
 
-            try
-            {
-                using (var fileCreation = new System.IO.FileStream(fileName, System.IO.FileMode.Create, System.IO.FileAccess.Write))
-                using (var writeOnFile = new System.IO.StreamWriter(fileCreation))
-                {
-                    byte data = (byte)'A';
-                    int bytesWritten = 0;
-
-                    while(bytesWritten < fileSize)
-                    {
-                        fileCreation.WriteByte(data);
-                        bytesWritten += 1;
-                    }
-                }
-                Console.WriteLine("File created successfully");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error creating the file: {ex.Message}");
-            }
+          
         }
     }
 }
